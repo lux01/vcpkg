834d5f37e466d0abf629989364a10d2e840aa600
William Marlow <william.marlow@ibm.com>
2025-11-08 19:12:22 +0000

    Add support for AIX

---
+++ triplets/community/ppc64-aix.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE ppc64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME AIX)
+set(VCPKG_C_FLAGS "-march=ppc64")
+set(VCPKG_CXX_FLAGS "-march=ppc64")
+set(VCPKG_LINKER_FLAGS "-march=ppc64")
